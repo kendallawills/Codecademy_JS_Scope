681a7d50445833a7d31b72104bc394725cf72db6
Kendall <kendallawills@gmail.com>
2023-07-15 17:04:44 -0400

    Global Scope

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ const logCitySkyline = () => {
 }
 
 console.log(logCitySkyline());
+
+// Global Scope
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+
+const callMyNightSky = () => {
+    return `Night Sky: ${satellite}, ${stars}, and ${galaxy}`;
+}
+
+console.log(callMyNightSky());
